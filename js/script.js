e85e433b319f0f84429d1e1e1856ab7a9c0dc489
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-05-22 16:05:20 -0400

    Finished Lab

--- js/script.js
+++ js/script.js
@@ -9,14 +9,75 @@ function PetChat(){
   };
 
   var askPetInfo = function(){
+    var textOut = "";
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
-      display.innerHTML += textOut;
+      if(type == "cat"){
+        var cat = new Cat(name);
+        textOut += cat.getName()+" says "+cat.speak()+"</br>";
+      }
+      if(type == "dog"){
+        var dog = new Dog(name);
+        textOut += dog.getName()+" says "+dog.speak()+"</br>";
+      }
+      if(type == "bird"){
+        var bird = new Bird(name);
+        textOut += bird.getName()+" says "+bird.speak()+"</br>";
+      }
+      display.innerHTML = textOut;
     }
   };
 
+function Pet(name){
+    this.name = name;
+};
+Pet.prototype.speak = function(){
+  return "I'm a pet";
+};
+Pet.prototype.getName = function(){
+  return this.name;
+};
+
+Cat.prototype = Object.create(Pet.prototype);
+Cat.prototype.constructor = Cat;
+
+function Cat(name){
+  Pet.call(this,name);
+};
+Cat.prototype.speak = function(){
+  return "Meow!";
+};
+
+Cat.prototype = Object.create(Pet.prototype);
+Cat.prototype.constructor = Cat;
+
+function Cat(name){
+  Pet.call(this,name);
+};
+Cat.prototype.speak = function(){
+  return "Meow!";
+};
+
+Dog.prototype = Object.create(Pet.prototype);
+Dog.prototype.constructor = Dog;
+
+function Dog(name){
+  Pet.call(this,name);
+};
+Dog.prototype.speak = function(){
+  return "Bark!";
+};
+
+Bird.prototype = Object.create(Pet.prototype);
+Bird.prototype.constructor = Bird;
+
+function Bird(name){
+  Pet.call(this,name);
+};
+Bird.prototype.speak = function(){
+  return "Chirp!";
+};
   this.init = function(){
     while(numPets === null){
       numPets = askHowMany();
